6d54b73645dc8687566f623916601f47b6b53564
Nusratulloh202 <nusratulloh202@gmail.com>
2022-10-21 22:01:07 +0500

    INFRA:upload Project

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-char smolla='a';
-char capitalA='A';
-System.Console.WriteLine((int)smolla);
-System.Console.WriteLine((int)capitalA);
-
-
-
-
-
-
-
-
-
-
-
+System.Console.Write("Hurmatli abanent ismingizni kiriting >>> ");
+string fullName=Console.ReadLine();
+System.Console.Write($"Assalomu alaykum {fullName} yoshingizni kiriting >>> ");
+int age =Convert.ToInt32(Console.ReadLine());
+string ageChek=
+      age >= 18 && age < 55
+        ?  $"{fullName} siz saytga kirishingiz mumkin!"
+        
+        :  $"{fullName} siz saytga kira olmesiz ";
+     System.Console.WriteLine(ageChek);   
+if (age>=18 && age<=55)
+{
+    string check = fullName switch
+{
+    
+    "Nusratulloh" =>$"Hurmatli {fullName} siz kompaniya ho'jayinisiz.\nHush kelibsiz kuninggiz xayrli o'tsin",
+    "Shahboz" => $"Hurmatli {fullName} siz kompaniya admenisiz\nHush kelibsiz kuninggiz xayrli o'tsin",
+    _ => $"Hurmatli {fullName} siz bu saytda foydalanuvchisiz"
+};
+System.Console.WriteLine(check);
+}
 
 
 
